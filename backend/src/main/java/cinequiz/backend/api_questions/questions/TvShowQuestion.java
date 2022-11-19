1b3be22f8de59850c301f22975ae52e4d06db06b
Emma <auzi.emma@gmail.com>
2022-11-19 15:31:01 +0100

    Fautes orthographe questions

--- backend/src/main/java/cinequiz/backend/api_questions/questions/TvShowQuestion.java
+++ backend/src/main/java/cinequiz/backend/api_questions/questions/TvShowQuestion.java
@@ -5,20 +5,20 @@ public class TvShowQuestion extends Question {
                 super(fr, en);
         }
 
-        public static final MovieQuestion WHICH_BY_IMAGE = new MovieQuestion("Quel est cette série?",
+        public static final MovieQuestion WHICH_BY_IMAGE = new MovieQuestion("Quelle est cette série ?",
                         "What is this series?");
         public static final MovieQuestion WHICH_BY_DESCRIPTION = new MovieQuestion(
-                        "A quel film correspond a cette description?",
+                        "A quel film correspond a cette description ?",
                         "Which film fits this description?");
-        public static final MovieQuestion TAKE_PART = new MovieQuestion("Quelle personne a participé à cette série?",
+        public static final MovieQuestion TAKE_PART = new MovieQuestion("Quelle personne a participé à cette série ?",
                         "Who participated in this series?");
         public static final MovieQuestion DOESNT_TAKE_PART = new MovieQuestion(
-                        "Quelle personne n'a pas participé à cette série?",
+                        "Quelle personne n'a pas participé à cette série ?",
                         "Who hasn't participated in this series?");
         public static final MovieQuestion RELEASE_DATE = new MovieQuestion(
-                        "Quelle a été la date de première diffusion de cette série?",
+                        "Quelle a été la date de première diffusion de cette série ?",
                         "When did this series first air?");
         public static final MovieQuestion HOW_MANY_EPISODES = new MovieQuestion(
-                        "Combien d'épisodes compte cette série?",
+                        "Combien d'épisodes compte cette série ?",
                         "How many episodes does this series have?");
 }
