30f3beec133c1e427b327a415037ced89d14f630
Emma <auzi.emma@gmail.com>
2022-11-21 15:05:05 +0100

    Mise en place ApiOperation

--- backend/src/main/java/cinequiz/backend/api_questions/controllers/PeopleQuestionController.java
+++ backend/src/main/java/cinequiz/backend/api_questions/controllers/PeopleQuestionController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/cinequiz/questions/people", method = RequestMethod.GET)
 public class PeopleQuestionController {
-
+    
+    @ApiOperation(value = "Gets a random mcq about a person")
     @GetMapping("/")
     public String random_question() {
         return "people";
--- backend/src/main/java/cinequiz/backend/api_questions/controllers/TvShowQuestionController.java
+++ backend/src/main/java/cinequiz/backend/api_questions/controllers/TvShowQuestionController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/cinequiz/questions/tv-show", method = RequestMethod.GET)
 public class TvShowQuestionController {
 
+    @ApiOperation(value = "Gets a random mcq about a tv show")
     @GetMapping("/")
     public String random_question() {
         return "tv-show";
