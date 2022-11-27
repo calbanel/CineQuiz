aaef02209437840626b391bd57d171e07207794c
Emma <auzi.emma@gmail.com>
2022-11-27 16:38:11 +0100

    Création composant question

--- frontend/AngularApplication/src/app/app.component.ts
+++ frontend/AngularApplication/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularApplication';
+  title = 'CineQuiz';
+}
+
+@Component({
+  selector: 'question',
+  templateUrl: './question/question.component.html',
+  styleUrls: ['./question/question.component.css']
+})
+export class QuestionComponent {
+  title = 'QuestionPage';
 }
