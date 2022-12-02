4f8e1a12c179f26949a88850f9dd672d8c059046
Emma <auzi.emma@gmail.com>
2022-12-02 12:45:15 +0100

    Changement couleur de fond en fonction de la réponse choisie

--- frontend/AngularApplication/src/app/question/question.component.ts
+++ frontend/AngularApplication/src/app/question/question.component.ts
@@ -9,21 +9,22 @@ export class QuestionComponent {
   title='Question';
   questionNumber:number = 1;
   type:string = "wich-by-image";
-  question: string|null = "Quel est ce film ?";
-  description: string|null = "Une charmante petite ville de l'Angleterre rurale. Si elle est originale et artiste dans l'âme, Louisa «Lou» Clark, 26 ans, n'a aucune ambition particulière. Elle se contente d'enchaîner les boulots pour permettre à ses proches de joindre les deux bouts. Jeune et riche banquier, Will Traynor était un garçon plein d'audace et d'optimisme jusqu'à ce qu'il se retrouve paralysé, suite à un accident survenu deux ans plus tôt. Devenu cynique, il a renoncé à tout et n'est plus que l'ombre de lui-même. Autant dire que ces deux-là auraient pu ne jamais se rencontrer. Mais lorsque Lou accepte de travailler comme aide-soignante auprès de Will, elle est bien décidée à lui redonner goût à la vie. Et peu à peu, les deux jeunes gens s'éprennent passionnément l'un de l'autre. La force de leur amour pourra-t-elle survivre à leur destin qui semble inexorable?";
-  answerA: string|null = "Un homme d’exception";
-  answerB: string|null = "Invincible : Le chemin de la rédemption";
-  answerC: string|null = "21 grammes";
-  answerD: string|null = "Sissi Impératrice";
-  answer: string|null = "Invincible : Le chemin de la rédemption";
+  question: string = "Quel est ce film ?";
+  description: string = "Une charmante petite ville de l'Angleterre rurale. Si elle est originale et artiste dans l'âme, Louisa «Lou» Clark, 26 ans, n'a aucune ambition particulière. Elle se contente d'enchaîner les boulots pour permettre à ses proches de joindre les deux bouts. Jeune et riche banquier, Will Traynor était un garçon plein d'audace et d'optimisme jusqu'à ce qu'il se retrouve paralysé, suite à un accident survenu deux ans plus tôt. Devenu cynique, il a renoncé à tout et n'est plus que l'ombre de lui-même. Autant dire que ces deux-là auraient pu ne jamais se rencontrer. Mais lorsque Lou accepte de travailler comme aide-soignante auprès de Will, elle est bien décidée à lui redonner goût à la vie. Et peu à peu, les deux jeunes gens s'éprennent passionnément l'un de l'autre. La force de leur amour pourra-t-elle survivre à leur destin qui semble inexorable?";
+  answerA: string = "Un homme d’exception";
+  answerB: string = "Invincible : Le chemin de la rédemption";
+  answerC: string = "21 grammes";
+  answerD: string = "Sissi Impératrice";
+  answer: string = "answerB";
   media: string|null = "https://image.tmdb.org/t/p/w500/6nh02WC31a5Sg3HqK21JY1nHqFo.jpg";
 
-  onClick(answerClicked:any){
+  onClick(answerClicked:string){
     console.log(answerClicked);
     if(answerClicked==this.answer){
-      alert("Bonne reponse");
+      document.getElementById(answerClicked)?.setAttribute("style", "background-color:#78e08f");
     }else{
-      alert("Mauvaise reponse");
+      document.getElementById(answerClicked)?.setAttribute("style", "background-color:#E55039");
+      document.getElementById(this.answer)?.setAttribute("style", "background-color:#78e08f");
     }
 
     
