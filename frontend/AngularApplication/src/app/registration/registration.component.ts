698ad3fdf56f99e5695c0c7f5ce9882c14bc64bd
Emma <auzi.emma@gmail.com>
2022-12-06 12:18:03 +0100

    Mise en place reactiveForm d'inscription

--- frontend/AngularApplication/src/app/registration/registration.component.ts
+++ frontend/AngularApplication/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registrationForm !: FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      pseudo: [null],
+      email: [null],
+      password: [null],
+      confirmPassword: [null]
+    });
+  }
+
+  onSubmitForm() : void {
+    console.log(this.registrationForm.value);
   }
 
 }
