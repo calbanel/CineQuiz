c8b3846da48ebc436049483e15c956cd24153db3
Emma <auzi.emma@gmail.com>
2022-12-09 21:22:23 +0100

    Validators pour les champs de formulaire

--- frontend/AngularApplication/src/app/connection/connection.component.ts
+++ frontend/AngularApplication/src/app/connection/connection.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connection',
@@ -14,8 +14,8 @@ export class ConnectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.connectionForm = this.formBuilder.group({
-      pseudo: [null],
-      password: [null]
+      pseudo: [null, Validators.required],
+      password: [null, Validators.required]
     });
   }
 
--- frontend/AngularApplication/src/app/registration/registration.component.ts
+++ frontend/AngularApplication/src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
@@ -14,10 +14,10 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.registrationForm = this.formBuilder.group({
-      pseudo: [null],
-      email: [null],
-      password: [null],
-      confirmPassword: [null]
+      pseudo: [null, [Validators.required]],
+      email: [null,  [Validators.required]],
+      password: [null,  [Validators.required]],
+      confirmPassword: [null,  [Validators.required]]
     });
   }
 
