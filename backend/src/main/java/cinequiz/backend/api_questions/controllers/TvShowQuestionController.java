d9d448817d4867ca07cfbdf486dc1ed1cbc0fe04
Clément Albanel <clement.albanel@gmail.com>
2022-12-24 17:55:34 +0100

    CrossOrigin TvShowController

--- backend/src/main/java/cinequiz/backend/api_questions/controllers/TvShowQuestionController.java
+++ backend/src/main/java/cinequiz/backend/api_questions/controllers/TvShowQuestionController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/cinequiz/questions/tv-show", method = RequestMethod.GET)
 public class TvShowQuestionController {
