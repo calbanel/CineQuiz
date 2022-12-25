eb73bac58f33dd2404396eb820c2480f99912a31
Clément Albanel <clement.albanel@gmail.com>
2022-12-25 17:03:36 +0100

    Creation des différentes strategy

---
+++ backend/src/main/java/cinequiz/backend/api_questions/utils/PersonStrategy.java
@@ -0,0 +1,37 @@
+package cinequiz.backend.api_questions.utils;
+
+import cinequiz.backend.api_questions.schemas.MCQQuestion;
+
+public class PersonStrategy implements MCQStrategy {
+
+    @Override
+    public MCQQuestion whichByImage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MCQQuestion whichByDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MCQQuestion date() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MCQQuestion takePart() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MCQQuestion doesntTakePart() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
