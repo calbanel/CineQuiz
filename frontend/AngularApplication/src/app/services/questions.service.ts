b9672a67d5b1390103b2c8184dd55739ebbf3415
Emma <auzi.emma@gmail.com>
2022-12-29 16:06:17 +0100

    Ajout méthode pour récupérer plusieurs questions depuis le front

--- frontend/AngularApplication/src/app/services/questions.service.ts
+++ frontend/AngularApplication/src/app/services/questions.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Question } from "../models/question.model";
 import { Observable } from "rxjs";
@@ -14,6 +14,11 @@ export class QuestionService {
 
   getQuestion(): Observable<Question> {
     return this.http.get<Question>("http://localhost:8080/api/questions/random");
+  }
+
+  getQuestionList(nb:number): Observable<Question[]> {
+    let queryParams = new HttpParams().append("number",nb);
+    return this.http.get<Question[]>("http://localhost:8080/api/questions/random-list",{params:queryParams});
   }
 
 }
