5800adc3123c07ce4933d498455999be6daa3112
Clément Albanel <clement.albanel@gmail.com>
2023-01-03 21:17:07 +0100

    Fix bug /credits

--- frontend/AngularApplication/src/app/footer/footer.component.ts
+++ frontend/AngularApplication/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goToCredits(){
+    this.router.navigateByUrl("/credits");
+  }
 
 }
