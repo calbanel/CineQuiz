1b944057f88aa69eb78b8a0c95d8a76b17709b62
Clément Albanel <clement.albanel@gmail.com>
2023-01-04 02:51:05 +0100

    Quelques refonte UI

--- frontend/AngularApplication/src/app/homepage/homepage.component.ts
+++ frontend/AngularApplication/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { GameService } from '../services/game.service';
 
 
@@ -8,9 +9,13 @@ import { GameService } from '../services/game.service';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
-    constructor(private game: GameService){
+    constructor(private game: GameService, private router: Router){
       if(this.game.gameLaunched){
         this.game.reset();
       }
+    }
+
+    loading(){
+      this.router.navigateByUrl("/loading");
     }
 }
