7132ed2b79183df8c0e8867e15bfbc182e8b620a
Clément Albanel <clement.albanel@gmail.com>
2023-01-12 02:00:12 +0100

    Seul les 10 dernieres games sont save

--- backend/src/main/java/cinequiz/backend/model/User.java
+++ backend/src/main/java/cinequiz/backend/model/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.GenerationType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.GeneratedValue;
@@ -31,10 +32,22 @@ public class User {
     @ApiModelProperty("The user password")
     private String password;
 
+    private static final int NB_GAMES_MAX = 10;
     @ApiModelProperty("The user played games")
     private List<Game> games;
 
     public void addGame(Game game) {
         games.add(game);
+
+        if (this.games.size() > NB_GAMES_MAX) {
+            this.games.remove(0);
+
+            List<Game> newList = new ArrayList<>();
+            for (Game copy : this.games) {
+                newList.add(copy);
+            }
+
+            this.games = newList;
+        }
     }
 }
