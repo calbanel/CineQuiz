3b2cb9ed782f10edcd5b19f79e3c568780b97c01
Clément Albanel <clement.albanel@gmail.com>
2023-01-14 19:59:52 +0100

    Affichage historique de partie

--- frontend/AngularApplication/src/app/homepage/homepage.component.ts
+++ frontend/AngularApplication/src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Game } from '../models/game.models';
+import { AccountService } from '../services/account.service';
 import { GameService } from '../services/game.service';
 
 
@@ -8,14 +10,30 @@ import { GameService } from '../services/game.service';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
-    constructor(private game: GameService, private router: Router){
-      if(this.game.gameLaunched){
-        this.game.reset();
+export class HomepageComponent{
+    gameHistory !: Game[];
+    selectedGame ?: Game;
+
+    constructor(private user: AccountService, private gameService : GameService, private router: Router){
+      if(this.gameService.gameLaunched){
+        this.gameService.reset();
       }
+
+      this.gameHistory = user.userValue.games;
     }
 
     loading(){
       this.router.navigateByUrl("/loading");
+    }
+
+    changeGame(e: any){
+      if(e.target.value == "null")
+        this.selectedGame = undefined;
+      else
+        this.selectedGame = this.gameHistory.find(value => value.date === e.target.value);
+
+      let carousel : any = document.getElementById("carousel-1");
+      if(carousel != null)
+        carousel.checked = true;
     }
 }
--- frontend/AngularApplication/src/environments/environment.ts
+++ frontend/AngularApplication/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: `${location.origin}/api`,
+  apiUrl: `http://localhost:8080/api`,
   nbQuestionsInQuiz: 10
 };
 
